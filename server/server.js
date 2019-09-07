227eaf126dd49268bba601043ffcc2fb47b2c767
Jessica Woudsma <jessicawoudsma@jssckbl.local>
2019-09-07 10:58:19 -0500

    updated server side

--- server/server.js
+++ server/server.js
@@ -2,13 +2,23 @@
 
 const express = require( 'express' );
 const app = express();
+const bodyParser = require('body-parser');
 
 // globals
 const port = 5000;
 
-// spin up server
-app.use( express.static( 'server/public' ) );
+///- let things = [];   <---- could this be the array that the calculator pushes into?
 
+// test data
+///- things.push( { name: 'bottle' } );  <---- this would need to be changed to go into the <ul>#equationsOut in index?
+
+
+// uses
+app.use( express.static( 'server/public' ) ); 
+app.use( bodyparser.urlencoded( { extended: true } ) ); /// <--- this should be totally fine
+///- should I have more app.use lines? 
+
+// spin up server
 app.listen( port, ()=>{
     console.log( 'server is up:', port );
 }) // end server up
